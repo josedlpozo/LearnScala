435580c5503f588b774992b53aa05eeaf72f1fca
Jose del Pozo <josedlpozo@gmail.com>
2018-04-06 17:56:29 +0200

    Add lesson 4 from week 3

---
+++ week4/src/main/scala/lesson4/List.scala
@@ -0,0 +1,35 @@
+package lesson4
+
+trait List[+T] {
+  def isEmpty: Boolean
+  def head: T
+  def tail: List[T]
+
+  def prepend[U >: T](elem: U): List[U] = new Cons(elem, this)
+}
+
+class  Cons[T](val head: T, val tail: List[T]) extends List[T] {
+  def isEmpty: Boolean = false
+}
+
+object Nil extends List[Nothing] {
+  def isEmpty = false
+
+  def head: Nothing = throw new NoSuchElementException("Nil.head")
+
+  def tail: Nothing = throw new NoSuchElementException("Nil.tail")
+}
+
+object List {
+
+  def apply[T]() = Nil[T]
+
+  def apply[T](x1: T) = new Cons[T](x1, Nil[T])
+
+  def apply[T](x1: T, x2: T) = new Cons[T](x1, new Cons[T](x2, Nil[T]))
+
+}
+
+object test {
+  val x: List[String] = Nil
+}
